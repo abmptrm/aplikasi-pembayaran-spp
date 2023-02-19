f93c245aa6a525b0a22c3dc35fcb2f20dbc1125c
ikann666 <adilbimoadil123@gmail.com>
2023-02-19 17:26:27 +0700

    cg siswa & petugas

--- proses-login-siswa.php
+++ proses-login-siswa.php
@@ -2,30 +2,30 @@
 
 include 'koneksi.php';
 
-if (isset($_POST['masuk-masyarakat'])) {
+
     # code...
-    $nisn = $_POST['nisn'];
-    $nis = $_POST['nis'];
-    
-    
-    $sql = "SELECT * FROM siswa WHERE nisn='$nisn' AND nis='$nis'";
-    $query = mysqli_query($koneksi,$sql);
-    
-    if(mysqli_num_rows($query)>0){
-        $data = mysqli_fetch_array($query);
-    
-        session_start();
-    
-        $_SESSION['nisn'] = $nisn;
-        $_SESSION['nis'] = $nis;
-        $_SESSION['nama'] = $data['nama'];
-    
-        header('location: siswa/siswa.php');
-    
-    }else{
-        echo"<script>alert('Maaf Login Gagal, Silahkan ulangi Lagi'); window.location.assign('index.php');
-        </script>";
-    }
+$nisn = $_POST['nisn'];
+$nis = $_POST['nis'];
+
+
+$sql = "SELECT * FROM siswa WHERE nisn='$nisn' AND nis='$nis'";
+$query = mysqli_query($koneksi,$sql);
+
+if(mysqli_num_rows($query)>0){
+    $data = mysqli_fetch_array($query);
+
+    session_start();
+
+    $_SESSION['nisn'] = $nisn;
+    $_SESSION['nis'] = $nis;
+    $_SESSION['nama'] = $data['nama'];
+
+    header('location: siswa/siswa.php');
+
+}else{
+    echo"<script>alert('Maaf Login Gagal, Silahkan ulangi Lagi'); window.location.assign('index.php');
+    </script>";
 }
+
 
     
